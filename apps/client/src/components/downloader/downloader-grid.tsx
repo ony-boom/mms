591f2967abfbf7f53ca2131b3680a4ce4ed40637
Titosy Manankasina <dev.titosy@gmail.com>
2025-03-05 09:42:09 +0300

    feat: implement DownloaderGrid and DownloadTrackCard components for improved track downloading UI

--- apps/client/src/components/downloader/downloader-grid.tsx
+++ apps/client/src/components/downloader/downloader-grid.tsx
@@ -1,11 +1,34 @@
+import { VirtuosoGrid, type GridComponents } from "react-virtuoso";
 import { DownloaderTrackItem } from "./downloader-track-item";
+import { forwardRef } from "react";
+
+const components: GridComponents = {
+  List: forwardRef(({ children, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+      >
+        {children}
+      </div>
+    );
+  }),
+  Item: ({ children, ...props }) => (
+    <div {...props} className="w-full">
+      {children}
+    </div>
+  ),
+};
 
 export const DownloaderGrid = () => {
   return (
-    <div className="grid grid-cols-1 gap-2 p-2 md:grid-cols-2 md:gap-3 md:p-3 lg:grid-cols-3 lg:gap-4 lg:p-4 xl:grid-cols-4">
-      {Array.from({ length: 24 }, (_, index) => (
-        <DownloaderTrackItem key={index} index={index} />
-      ))}
-    </div>
+    <VirtuosoGrid
+      data={Array.from({ length: 100 }, (_, i) => i)}
+      overscan={2}
+      components={components}
+      itemContent={(index) => <DownloaderTrackItem index={index} />}
+      style={{ height: "100%", willChange: "transform" }}
+    />
   );
 };
--- apps/client/src/components/downloader/index.tsx
+++ apps/client/src/components/downloader/index.tsx
@@ -1,58 +1,28 @@
-import { forwardRef, memo } from "react";
+import { memo } from "react";
 import { DrawerContent, DrawerHeader } from "../ui/drawer";
 import { Input } from "../ui/input";
-import { DownloaderTrackItem } from "./downloader-track-item";
-import { VirtuosoGrid } from "react-virtuoso";
 import { Separator } from "../ui/separator";
+import { DialogTitle } from "../ui/dialog";
+import { DownloaderGrid } from "./downloader-grid";
 
 export const Downloader = memo(() => {
   return (
     <DrawerContent>
       <div className="flex h-[60vh] w-full flex-col">
         <DrawerHeader>
-          <Input
-            placeholder="Search track to download..."
-            className="border-b-foreground/10 focus-visible:border-b-foreground/30 min-w-xl h-12 max-w-sm rounded-none focus-visible:ring-0"
-            onClick={(e) => e.stopPropagation()}
-          />
+          <DialogTitle>
+            <Input
+              placeholder="Search track to download..."
+              className="border-b-foreground/10 focus-visible:border-b-foreground/30 min-w-xl h-12 max-w-sm rounded-none focus-visible:ring-0"
+              onClick={(e) => e.stopPropagation()}
+            />
+          </DialogTitle>
         </DrawerHeader>
 
         <Separator className="bg-foreground/[8%]" />
 
         <div className="flex-1">
-          <VirtuosoGrid
-            data={Array.from({ length: 100 }, (_, i) => i)}
-            overscan={2}
-            components={{
-              List: forwardRef<
-                HTMLDivElement,
-                { style?: React.CSSProperties; children?: React.ReactNode }
-              >(({ style, children, ...props }, ref) => {
-                return (
-                  <div
-                    ref={ref}
-                    {...props}
-                    style={{
-                      ...style,
-                      display: "grid",
-                      gap: "16px",
-                      padding: "16px",
-                    }}
-                    className="grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-                  >
-                    {children}
-                  </div>
-                );
-              }),
-              Item: ({ children, ...props }) => (
-                <div {...props} className="w-full">
-                  {children}
-                </div>
-              ),
-            }}
-            itemContent={(index) => <DownloaderTrackItem index={index} />}
-            style={{ height: "100%", willChange: "transform" }}
-          />
+          <DownloaderGrid />
         </div>
       </div>
     </DrawerContent>
