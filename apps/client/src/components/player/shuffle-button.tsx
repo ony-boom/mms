00c2d6be05fd24f77ce84ff9547ee6c1d5585d51
Ony <onyrakoto27@gmail.com>
2025-03-06 20:57:42 +0300

    fix some state access inside player

--- apps/client/src/components/player/shuffle-button.tsx
+++ apps/client/src/components/player/shuffle-button.tsx
@@ -2,10 +2,17 @@ import { cn } from "@/lib/utils.ts";
 import { Shuffle } from "lucide-react";
 import { usePlayerStore } from "@/stores";
 import { Button, ButtonProps } from "@/components/ui/button.tsx";
+import { useShallow } from "zustand/react/shallow";
 
 export function ShuffleButton(props: ShuffleButtonProps) {
-  const { toggleShuffle, isShuffle, getCurrentPlaylist } =
-    usePlayerStore.getState();
+  const { toggleShuffle, isShuffle, getCurrentPlaylist } = usePlayerStore(
+    useShallow((state) => ({
+      toggleShuffle: state.toggleShuffle,
+      isShuffle: state.isShuffle,
+      getCurrentPlaylist: state.getCurrentPlaylist,
+    })),
+  );
+
   const handleShuffle = () => {
     toggleShuffle();
   };
