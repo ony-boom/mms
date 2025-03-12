87df9d576f0377e160ec151b32005bc7482967ef
Ony <onyrakoto27@gmail.com>
2025-03-12 07:46:34 +0300

    feat: update build configuration and scripts, remove nexe dependency

--- packages/bin/package/package.ts
+++ packages/bin/package/package.ts
@@ -1,27 +1,7 @@
 import { build } from "./build";
-import { compile } from "nexe";
-import * as path from "node:path";
-import { serverDir } from "../constants";
 
 async function packageApp() {
   await build();
-
-  await compile({
-    name: "mms",
-    build: true,
-    // TODO: find a way to make this more dynamic
-    python: "/usr/bin/python3",
-    // TODO: add more targets
-    targets: [{ arch: "x64", platform: "linux", version: "20.18.3" }],
-    input: path.join(serverDir, ".output", "server", "index.mjs"),
-    remote: 'https://github.com/urbdyn/nexe_builds/releases/download/0.4.0/',
-    verbose: true,
-    resources: [
-      path.join(serverDir, ".output", "public"),
-      path.join(serverDir, ".output", "public"),
-      path.join(serverDir, ".output", "nitro.json"),
-    ],
-  });
 }
 
 packageApp().catch(console.error);
