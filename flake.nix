c09408bfc8fd53890286b0300f30d26510737e65
Ony <onyrakoto27@gmail.com>
2025-03-14 18:09:47 +0300

    separate nix file

--- flake.nix
+++ flake.nix
@@ -10,44 +10,24 @@
     self,
     nixpkgs,
     flake-utils,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages = {
-          default = pkgs.stdenv.mkDerivation (finalAttrs: {
-            pname = "mms";
-            src = ./.;
-            version = "0.0.1";
-
-            dontUnpack = true;
-
-            buildInputs = [pkgs.nodejs pkgs.pnpm];
-            nativeBuildInputs = [pkgs.nodejs pkgs.pnpm.configHook];
-
-            pnpmDeps = pkgs.pnpm.fetchDeps {
-              inherit
-                (finalAttrs)
-                pname
-                version
-                src
-                ;
-              hash = "sha256-UpLfCixNiTGmoQGslkkphz1XC9UCKSiKxvneUy07sag=";
-            };
-
-            buildPhase = ''
-              runHook preBuild
-              pnpm build
-              runHook postBuild
-            '';
-
-            installPhase = ''
-              mkdir -p $out
-              cp -r build/* $out
-            '';
-          });
+          default = import ./nix/package.nix {inherit pkgs;};
         };
       }
-    );
+    )
+    // {
+      # Home Manager module
+      homeManagerModules.x86_64-linux = import ./nix/modules/home-manager.nix;
+
+      # For convenience, expose the module as an overlay too
+      overlays.default = final: prev: {
+        mms = self.packages.${prev.system}.default;
+      };
+    };
 }
