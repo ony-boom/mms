d3758d2f0f0bfaaeffc56fb526b1d6ae3dfc8494
Ony <onyrakoto27@gmail.com>
2025-05-02 20:41:28 +0300

    add nixos module

---
+++ nix/modules/nixos.nix
@@ -0,0 +1,49 @@
+{mms}: {
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.services.mms;
+in {
+  options.services.mms = {
+    enable = mkEnableOption "Enable the MMS system service.";
+
+    package = mkOption {
+      type = types.package;
+      default = mms;
+      description = "The MMS package to use.";
+    };
+
+    port = mkOption {
+      type = types.port;
+      default = 3536;
+      description = "Port on which MMS will listen.";
+    };
+
+    host = mkOption {
+      type = types.str;
+      default = "localhost";
+      description = "Host on which MMS will listen.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [cfg.package];
+
+    systemd.services.mms = {
+      description = "MMS Service";
+      after = ["network.target"];
+      wantedBy = ["multi-user.target"];
+
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/mms";
+        Restart = "on-failure";
+        Environment = [
+          "PORT=${toString cfg.port}"
+          "HOST=${toString cfg.host}"
+        ];
+      };
+    };
+  };
+}
