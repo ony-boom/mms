96866ed77d58656acc683fb46d2d800847935692
Ony <onyrakoto27@gmail.com>
2025-07-01 17:58:39 +0300

    flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "mms flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
