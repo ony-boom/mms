2c709bbb191d504aa041577afdf9b28a6d3a4e67
Ony <onyrakoto27@gmail.com>
2025-08-04 19:31:53 +0300

    added settings dialog

--- apps/client/src/components/settings-dilog.tsx
+++ apps/client/src/components/settings-dilog.tsx
@@ -1,11 +1,30 @@
-import { Dialog, DialogContent, DialogTitle } from "./ui/dialog";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { ComponentProps } from "react";
 
 export function SettingsDialog(props: SettingsDialogProps) {
   return (
     <Dialog {...props}>
-      <DialogContent className="bg-background">
-        <DialogTitle>Settings</DialogTitle>
+      <DialogContent className="bg-background max-h-[400px]">
+        <DialogHeader>
+          <DialogTitle>Settings</DialogTitle>
+        </DialogHeader>
+
+        <Accordion type="single" collapsible>
+          <AccordionItem value="appearance">
+            <AccordionTrigger>
+              <h3 className="font-bold">Appearance</h3>
+            </AccordionTrigger>
+            <AccordionContent>
+              Appearance settings will go here.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </DialogContent>
     </Dialog>
   );
